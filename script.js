91662751908f9d97d98378e04b68ea44ba48b44c
DeerajAlagarsamy <deerajalagarsamyol7xl9@gmail.com>
2023-11-18 09:42:24 +0530

    grid complete

---
+++ script.js
@@ -0,0 +1,12 @@
+function addiconclass() {
+  const carticon = document.getElementById("carticon");
+  carticon.classList.add("bx-tada");
+  //   carticon.style.color = "#fff";
+  //   console.log("sdfsdfad");
+}
+function removeiconclass() {
+  const carticon = document.getElementById("carticon");
+  carticon.classList.remove("bx-tada");
+  //   carticon.style.color = "#fff";
+  //   console.log("sdfsdfad");
+}
